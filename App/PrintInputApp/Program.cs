0b3c0eb52185a47573b9531a0d20c8d8499aa8f6
DESKTOP-10G9VFF\hasanak <nimble-nerd71@gmail.com>
2024-05-15 17:16:47 +0200

    Changed the folder structure

---
+++ App/PrintInputApp/Program.cs
@@ -0,0 +1,57 @@
+﻿using PrintInputApp;
+
+class Program
+{
+    /// <summary>
+    /// Defines the entry point of the application.
+    /// </summary>
+    /// <param name="args">The arguments.</param>
+    static void Main(string[] args)
+    {
+        bool exitRequested = false;
+
+        Console.WriteLine("Enter the input (type 'exit' to quit): ");
+
+        while (!exitRequested)
+        {
+            var input = Console.ReadLine();
+
+            if (input?.ToLower() == "clear" || input?.ToLower() == "cls")
+            {
+                Console.Clear();
+            }
+
+            if (input?.ToLower() == "exit")
+            {
+                Console.WriteLine("Exiting...");
+                exitRequested = true;
+            }
+            else
+            {
+                //This is an attempt to seperate validation from the actual input processing.
+
+                var validationIssues = Validation.IsValidInput(input);
+                if (validationIssues.Count > 0)
+                {
+                    foreach (var validationIssue in validationIssues)
+                    {
+                        Console.WriteLine(validationIssue);
+                    }
+                    continue;
+                }
+
+                var result = ProcessInput.GetNumbers(input);
+
+                if (result?.Any() != null)
+                {
+                    Console.WriteLine("Output: " + string.Join(",", result));
+                }
+            }
+        }
+
+        if (exitRequested)
+        {
+            Environment.Exit(0);
+        }
+    }
+}
---
+++ Tests/TestProject/PrintInputAppTests.cs
@@ -0,0 +1,25 @@
+namespace TestProject
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Given_input_when_is_empty_returns_validation_error()
+        {
+        }
+
+        [Fact]
+        public void Given_input_when_invalid_input_returns_validation_error()
+        {
+        }
+
+        [Fact]
+        public void Given_input_when_range_input_contains_wrong_data_returns_validation_error()
+        {
+        }
+
+        [Fact]
+        public void Given_input_when_valid_returns_a_list_to_print()
+        {
+        }
+    }
+}
