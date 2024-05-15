3d3b05768103fba1b02618b3f7a2b89301085af2
DESKTOP-10G9VFF\hasanak <nimble-nerd71@gmail.com>
2024-05-15 14:11:33 +0200

    Implemented the print input app

---
+++ Validation.cs
@@ -0,0 +1,70 @@
+﻿using System.Text.RegularExpressions;
+
+namespace PrintInputApp;
+internal static class Validation
+{
+    /// <summary>
+    /// Validate input params
+    /// </summary>
+    /// <param name="input">The input.</param>
+    /// <returns>A list of issues, if there are any</returns>
+    internal static IReadOnlyCollection<string> IsValidInput(string input)
+    {
+        var validationIssues = new List<string>();
+
+        if (string.IsNullOrEmpty(input))
+        {
+            validationIssues.Add("Input is empty.");
+        }
+
+        var inputData = input.Trim();
+
+        var pattern = @"^(\d+(-\d+)?,)*\d+(-\d+)?$";
+
+        bool IsInputValidated = true;
+
+        if (!Regex.IsMatch(inputData, pattern))
+        {
+            IsInputValidated = false;
+        }
+
+        foreach (char c in inputData)
+        {
+            if (!char.IsDigit(c) && c != '-' && c != ',')
+            {
+                IsInputValidated = false;
+                continue;
+            }
+        }
+
+        if (!IsInputValidated)
+        {
+            validationIssues.Add("Input contains invalid data.");
+        }
+
+        if (inputData.Contains('-') &&
+            !string.IsNullOrEmpty(ValdiateRangeInput(inputData)))
+        {
+            validationIssues.Add(ValdiateRangeInput(inputData));
+        }
+        return validationIssues.AsReadOnly();
+    }
+
+    private static string ValdiateRangeInput(string input)
+    {
+        var ranges = input.Split(',');
+        foreach (var range in ranges)
+        {
+            if (!range.Contains('-'))
+            {
+                continue;
+            }
+            var parts = range.Split('-');
+            if (parts.Length != 2)
+            {
+                return "Input contains invalid characters in range data.";
+            }
+        }
+        return string.Empty;
+    }
+}
